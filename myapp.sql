94e8fb19c6d642dcd552df25540843af4430a488
Saoki11111 <t.aoki11111@gmail.com>
2020-08-10 22:28:45 +0900

    内部結合

---
+++ myapp.sql
@@ -0,0 +1,32 @@
+drop table if exists posts;
+create table posts (
+  id int unsigned primary key auto_increment,
+  title varchar(255),
+  body text
+);
+drop table if exists comments;
+create table comments (
+  id int unsigned primary key auto_increment,
+  post_id int not null,
+  body text
+);
+
+insert into posts (title, body) values ('title 1', 'body 1');
+insert into posts (title, body) values ('title 2', 'body 2');
+insert into posts (title, body) values ('title 3', 'body 3');
+
+insert into comments (post_id, body) values (1, 'first comment for post 1');
+insert into comments (post_id, body) values (1, 'second comment for post 1');
+insert into comments (post_id, body) values (3, 'first comment for post 3');
+insert into comments (post_id, body) values (4, 'first comment for post 4');
+
+-- inner join
+-- outer join
+
+-- select * from posts inner join comments on posts.id = comments.post_id;
+-- # inner は省略できる
+-- select * from posts join comments on posts.id = comments.post_id;
+# 特定のフィールドのみ取得する
+-- select posts.id, posts.title, posts.body, comments.body from posts join comments on posts.id = comments.post_id;
+# フィールド名は自身のテーブルなら省略できる
+select posts.id, title, posts.body, comments.body from posts join comments on posts.id = comments.post_id;
