ee243b64f65d6cb8c75160a576585d35a8452f64
Saoki11111 <t.aoki11111@gmail.com>
2020-08-11 09:46:56 +0900

    git rm

--- create_myapp.sql
+++
@@ -1,5 +0,0 @@
-# myapp が存在するとエラーになるため 最初に削除する
-drop database if exists myapp;
-create database myapp;
-# myapp のすべてのテーブルに対してすべての権限をこちらのユーザーに与える
-grant all on myapp.* to myapp_user@localhost identified by '2VNAh1go';
